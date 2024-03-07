a16bc94f2c3c7e71a429218da4f21e00e1aa6d00
Pariansh Mahajan <116679976+ParianshMahajan@users.noreply.github.com>
2024-03-08 03:27:27 +0530

    bjchbf

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const bodyParser = require('body-parser');
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 
+const https = require('https');
+
+
+const server = https.createServer(app);
+
+
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
@@ -23,6 +29,6 @@ app.get('/', (req, res) => {
   
 let port=process.env.PORT;
   
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
