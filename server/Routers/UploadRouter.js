64e23ee411e525ab318d2668670f03fbfe471c3f
Pariansh Mahajan <116679976+ParianshMahajan@users.noreply.github.com>
2024-05-06 05:59:11 +0530

    hi

--- server/Routers/UploadRouter.js
+++ server/Routers/UploadRouter.js
@@ -11,6 +11,7 @@ const { createScript, startScript, stopScript, deleteScript } = require('../Cont
 const FrontendModel = require('../Models/FrontendModel');
 const { triggerScript } = require('../config/VM_Trigger');
 const { createDns } = require('../Controllers/CloudflareController');
+const { UploadZip, ProcessZip } = require('../Controllers/UploadController');
 
 const app = express();
 const UploadRouter = express.Router();
@@ -50,88 +51,7 @@ let headers = {
 
 UploadRouter
 .route('/')
-.post(upload.single('file'), async(req, res) => {
-    try {
-
-      
-      if (!req.file) {
-        
-        return res.json({
-          message:'No file was uploaded.',
-          status:false
-        });
-        
-        throw new Error('No file Uploaded');
-        
-      }
-
-      // Extracting JSON data 
-
-      const jsonData = JSON.parse(req.body.data);
-      const  fname = jsonData.fname;
-      const  framework = jsonData.framework;
-
-
-      //Creating dns
-      let dnsResult=await createDns(fname);
-      console.log(dnsResult);
-      if(dnsResult==false){
-        return res.json({
-          message:'DNS Creation Failed',
-          status:false
-        });
-        throw new Error('DNS Creation Failed');
-      }
-
-
-      // Extracting Files
-
-      const extractionDir = path.join(uploadDir, fname);
-      fs.mkdirSync(extractionDir, { recursive: true });
-
-      const zipFilePath = req.file.path;
-
-      // const zip = new AdmZip(zipFilePath);
-      // zip.extractAllTo(extractionDir);
-
-
-
-      exec(`unzip ${zipFilePath} -d ${extractionDir}`);
-      fs.unlink(zipFilePath, (unlinkErr) => {
-          if (unlinkErr) {
-            console.error("Error deleting temporary file:", unlinkErr);
-          }
-        });
-
-      //   Adding Scripts
-
-        createScript(path.join(extractionDir, 'create.sh'),fname,dnsResult.name,framework);
-        startScript(path.join(extractionDir, 'start.sh'),fname);
-        stopScript(path.join(extractionDir, 'stop.sh'),fname);
-        deleteScript(path.join(extractionDir, 'delete.sh'),fname);
-
-
-        let siteData={
-            SiteDNS:dnsResult.name,
-            DNSId:dnsResult.id,
-            fname:fname,
-            fpath:extractionDir
-        }
-
-        let site=await FrontendModel.create(siteData);
-        triggerScript(fname,20); 
-
-        res.json({
-          message: "Site Deployed Successfully",
-          status:true
-        });
-      
-      } catch (error) {
-        res.json({
-          message:error.message
-        })
-      }
-  });
+.post(upload.single('file'),UploadZip,ProcessZip);
 
 
 
