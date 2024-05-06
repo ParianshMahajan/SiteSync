da1959a52919d41017229adb1884a2f67e7e5c8c
Pariansh Mahajan <116679976+ParianshMahajan@users.noreply.github.com>
2024-05-06 06:06:50 +0530

    hi

--- Routers/UploadRouter.js
+++ Routers/UploadRouter.js
@@ -2,12 +2,16 @@ const express = require('express');
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
-const unzipper = require('unzipper');
+const AdmZip = require("adm-zip");
+const { exec } = require('child_process'); 
+
+
 const axios = require('axios');
 const { createScript, startScript, stopScript, deleteScript } = require('../Controllers/ScriptController');
 const FrontendModel = require('../Models/FrontendModel');
 const { triggerScript } = require('../config/VM_Trigger');
 const { createDns } = require('../Controllers/CloudflareController');
+const { UploadZip, ProcessZip } = require('../Controllers/UploadController');
 
 const app = express();
 const UploadRouter = express.Router();
@@ -47,96 +51,7 @@ let headers = {
 
 UploadRouter
 .route('/')
-.post(upload.single('file'), async(req, res) => {
-    try {
-
-      
-      if (!req.file) {
-        
-        return res.json({
-          message:'No file was uploaded.',
-          status:false
-        });
-        
-        throw new Error('No file Uploaded');
-        
-      }
-
-      // Extracting JSON data 
-
-      const jsonData = JSON.parse(req.body.data);
-      const  fname = jsonData.fname;
-      const  framework = jsonData.framework;
-
-
-      //Creating dns
-      let dnsResult=await createDns(fname);
-      console.log(dnsResult);
-      if(dnsResult==false){
-        return res.json({
-          message:'DNS Creation Failed',
-          status:false
-        });
-        throw new Error('DNS Creation Failed');
-      }
-
-
-      // Extracting Files
-
-      const extractionDir = path.join(uploadDir, fname);
-      fs.mkdirSync(extractionDir, { recursive: true });
-
-      const zipFilePath = req.file.path;
-
-      fs.createReadStream(zipFilePath)
-        .pipe(unzipper.Parse())
-        .on('entry', (entry) => {
-          const fileName = entry.path;
-          const destinationPath = path.join(extractionDir, fileName);
-
-          if (entry.type === 'Directory') {
-            fs.mkdirSync(destinationPath, { recursive: true });
-          } else {
-            entry.pipe(fs.createWriteStream(destinationPath));
-          }
-        })
-        .on('error', (error) => {
-          throw new Error('Error extracting zip file');
-        })
-        .on('finish', () => {
-          console.log('Zip file extracted successfully.');
-        });
-
-
-      //   Adding Scripts
-
-        await createScript(path.join(extractionDir, 'create.sh'),fname,dnsResult.name,framework);
-        await startScript(path.join(extractionDir, 'start.sh'),fname);
-        await stopScript(path.join(extractionDir, 'stop.sh'),fname);
-        await deleteScript(path.join(extractionDir, 'delete.sh'),fname);
-
-
-        let siteData={
-            SiteDNS:dnsResult.name,
-            DNSId:dnsResult.id,
-            fname:fname,
-            fpath:extractionDir
-        }
-
-        let site=await FrontendModel.create(siteData);
-        await triggerScript(fname,20); 
-      
-        res.json({
-          message: "Site Deployed Successfully",
-          status:true
-        });
-      
-      } catch (error) {
-        res.json({
-          message:error.message
-        })
-      }
-  });
+.post(upload.single('file'),UploadZip,ProcessZip);
 
 
 
