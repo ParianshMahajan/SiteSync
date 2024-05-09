2a40535f73c0258c1cdd7058b24c940e0d3da4c2
Pariansh Mahajan <116679976+ParianshMahajan@users.noreply.github.com>
2024-05-09 19:51:58 +0530

    Testing SubParts

--- Controllers/UploadController.js
+++ Controllers/UploadController.js
@@ -71,24 +71,14 @@ module.exports.ProcessZip = async (req, res) => {
         const zipFilePath = req.file.path;
 
         console.log(zipFilePath);
-        exec(`unzip -r ${zipFilePath} -d ${extractionDir}`, (error, stdout, stderr) => {
-            if (error) {
-                console.error("Error unzipping file:", error);
-                return res.status(500).json({
-                    message: "Error unzipping file"
-                });
-            }
-            console.log("stdout:", stdout);
-            console.error("stderr:", stderr);
-            fs.unlink(zipFilePath, (unlinkErr) => {
-                if (unlinkErr) {
-                    console.error("Error deleting temporary file:", unlinkErr);
-                }
-            });
-            res.json({
-                message: "Site Deployed Successfully",
-                status: true
-            });
+        
+        fs.createReadStream(zipFilePath)
+        .pipe(unzipper.Extract({ path: extractionDir }))
+        .on('error', err => {
+          console.error(`Error extracting zip file: ${err}`);
+        })
+        .on('finish', () => {
+          console.log('Zip file extracted successfully');
         });
         
 
