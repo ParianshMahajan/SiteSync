edb0b5d5713efff2a473fcf332ae1e5cd90f0d24
Pariansh Mahajan <116679976+ParianshMahajan@users.noreply.github.com>
2024-05-10 19:48:48 +0530

    Testing SubParts

--- Middlewares/Unzipper.js
+++ Middlewares/Unzipper.js
@@ -1,56 +1,21 @@
-const fs = require('fs');
-const path = require('path');
-const yauzl = require('yauzl');
+const { exec } = require('child_process');
 
-module.exports.extractZip = async function extractZip(zipFilePath, extractionDir) {
-    try {
-        await new Promise((resolve, reject) => {
-            yauzl.open(zipFilePath, { lazyEntries: true }, (err, zipfile) => {
-                if (err) {
-                    reject(err);
-                    return;
-                }
-
-                zipfile.readEntry();
-
-                zipfile.on('entry', entry => {
-                    const outputPath = path.join(extractionDir, entry.fileName);
-
-                    if (entry.fileName.endsWith('/')) {
-                        // Directory entry, create directory
-                        fs.mkdirSync(outputPath, { recursive: true });
-                        zipfile.readEntry();
-                    } else {
-                        // File entry, extract file
-                        zipfile.openReadStream(entry, (err, readStream) => {
-                            if (err) {
-                                reject(err);
-                                return;
-                            }
-
-                            readStream.on('error', err => reject(err));
-
-                            readStream.pipe(fs.createWriteStream(outputPath));
-                            readStream.on('end', () => {
-                                zipfile.readEntry();
-                            });
-                        });
-                    }
-                });
-
-                zipfile.on('end', () => {
-                    resolve();
-                });
-
-                zipfile.on('error', err => {
-                    reject(err);
-                });
-            });
+module.exports.extractZip = function extractZip(zipFilePath, extractionDir) {
+    return new Promise((resolve, reject) => {
+        const command = `unzip -o ${zipFilePath} -d ${extractionDir}`;
+        exec(command, (error, stdout, stderr) => {
+            if (error) {
+                console.error(`Extraction error: ${error.message}`);
+                reject(error);
+                return;
+            }
+            if (stderr) {
+                console.error(`Extraction stderr: ${stderr}`);
+                reject(new Error(stderr));
+                return;
+            }
+            console.log('Extraction complete');
+            resolve();
         });
-
-        console.log('Extraction complete');
-    } catch (error) {
-        console.error('Extraction error:', error);
-        throw error;
-    }
+    });
 };
