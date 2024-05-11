47054d9a8330cf941738386edf845a0099420a8e
Pariansh Mahajan <116679976+ParianshMahajan@users.noreply.github.com>
2024-05-12 00:35:19 +0530

    Running git hook

--- server/app.js
+++ server/app.js
@@ -23,7 +23,6 @@ const AdminRouter = require('./Routers/AdminRouter.js');
 startRoutes();
 
 
-
 let port = process.env.PORT || 2123;
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
