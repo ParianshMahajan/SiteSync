c2c7db0671864e2d5ce0253a9e4fc181a5c674ae
Pariansh Mahajan <116679976+ParianshMahajan@users.noreply.github.com>
2024-06-19 11:35:09 +0530

    Deloying

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const config = {
-    eslint: {
-      ignoreDuringBuilds: true,
-    },
-    typescript: {
-      // !! WARN !!
-      // Dangerously allow production builds to successfully complete even if
-      // your project has type errors.
-      // !! WARN !!
-      ignoreBuildErrors: true,
-    },
-  };
-  
-  export default config;
+const config = {};
+
+export default config;
